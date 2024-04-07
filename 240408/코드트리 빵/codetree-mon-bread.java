2849fef15040fc5bbcbcccd1592f6a3b97991a4c
dltjdn <78267146+dltjdn@users.noreply.github.com>
2024-04-08 02:01:09 +0900

    Title: 코드트리 빵, Time: 162ms, Memory: 17MB, Status: Passed - CodeTree

--- 240408/코드트리 빵/codetree-mon-bread.java
+++ 240408/코드트리 빵/codetree-mon-bread.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
 
 	static int n;
@@ -50,40 +49,67 @@ public class Main {
 			if(time>1) {
 				for(int i=1; i<Math.min(time,m+1); i++) { // 격자에 있는 사람모두
 					
+					
 					if(isReach[i]) continue;//이미 편의점 도달한 사람제
 					
 					Point b= starts[i];
 					Point m = markets[i];
 					
-					int minDir = -1;
-					int minDist = b.getDist(m.r, m.c);
+					Queue<Point> queue = new ArrayDeque<>();
+					Map<Point, Boolean> visited = new HashMap<>();
+					int[][] step = new int[n+1][n+1];
+					
+					queue.add(new Point(m.r, m.c)); // 편의점부터출
+					visited.put(new Point(m.r,m.c), true);
+					step[m.r][m.c] = 0;
+					
+					//System.out.println("11111");
+					while(!queue.isEmpty()) {
+						Point cur = queue.remove();
+						//System.out.println("222222");
+						
+						for(int dir=0; dir<4; dir++) {
+							int nextR = cur.r + dx[dir];
+							int nextC = cur.c + dy[dir];
+							Point next = new Point(nextR, nextC);
+							
+							if(next.inRange() && !visited.containsKey(next) && map[nextR][nextC] != -1) {
+								queue.add(next);
+								visited.put(next,true);
+								step[next.r][next.c] = step[cur.r][cur.c]+1;
+							}
+
+						}
+
+					}
+
+					
+					
+					int minX = -1;
+					int minY = -1;
+					int minDist = Integer.MAX_VALUE;
 					for(int dir=0; dir<4; dir++) {
 						int nextR = b.r + dx[dir];
 						int nextC = b.c + dy[dir];
 						Point next = new Point(nextR, nextC);
-						
-						if(!next.inRange() || map[nextR][nextC] == -1) continue;
-						
-						int dist = next.getDist(m.r,m.c); 
-						
-						if(dist < minDist) {
-							minDir = dir;
-							minDist = dist;
+						//System.out.println("3333");
+						if(next.inRange() && visited.containsKey(next) && step[nextR][nextC] < minDist) {
+							minDist = step[nextR][nextC];
+							minX = nextR;
+							minY = nextC;
 						}
-						
 					}
 					
-					if(minDir != -1) {
-						starts[i].r += dx[minDir];
-						starts[i].c += dy[minDir];
-					}
+					
+					starts[i] = new Point(minX, minY);
+
 					
 				}
-//				
-//				for(int i=1; i<Math.min(time,m+1); i++) {
-//					System.out.println("r: "+ baseCamps[i].r + " c: "+ baseCamps[i].c);
-//				}
-//				
+				
+				// for(int i=1; i<Math.min(time,m+1); i++) {
+				// 	System.out.println("r: "+ starts[i].r + " c: "+ starts[i].c);
+				// }
+			
 				// 2. 편의점에 도달  +  해당 칸 이동 불가능
 				for(int i=1; i<Math.min(time,m+1); i++) {
 					if(isReach[i]) continue;
@@ -103,33 +129,60 @@ public class Main {
 			
 			// 3. t번 사람 가까 있 베이스캠프 배정  + 해당 칸 이동불가능	
 			if(time <= m) {
-				int closestDist = n+n;
-				int closestR = n;
-				int closestC = n;
-	
-				Point p = markets[time];
 				
-				for(Point baseCamp : baseCamps){
-					//System.out.println("!!"+baseCamp);
-					if(map[baseCamp.r][baseCamp.c] == -1) continue;
+				Point m = markets[time];
+				
+				Queue<Point> queue = new ArrayDeque<>();
+				Map<Point, Boolean> visited = new HashMap<>();
+				int[][] step = new int[n+1][n+1];
+				
+				queue.add(new Point(m.r, m.c)); // 편의점부터출
+				visited.put(new Point(m.r,m.c), true);
+				step[m.r][m.c] = 0;
+				
+				while(!queue.isEmpty()) {
+					Point cur = queue.remove();
 					
-					int dist = p.getDist(baseCamp.r, baseCamp.c);
-					
-					if(dist < closestDist) {
-						closestDist = dist;
-						closestR = baseCamp.r;
-						closestC = baseCamp.c;
+					for(int dir=0; dir<4; dir++) {
+						int nextR = cur.r + dx[dir];
+						int nextC = cur.c + dy[dir];
+						Point next = new Point(nextR, nextC);
+						
+						if(next.inRange() && !visited.containsKey(next) && map[nextR][nextC] != -1) {
+							queue.add(next);
+							visited.put(next,true);
+							step[next.r][next.c] = step[cur.r][cur.c]+1;
+						}
+
 					}
+
+				}
+
+				
+				int minDist = Integer.MAX_VALUE;
+				int minX= -1, minY = -1;
+				for(Point b : baseCamps){
+					
+					if(map[b.r][b.c] == -1) continue;
+					
+					if(visited.containsKey(b) && step[b.r][b.c] < minDist) {
+						minDist = step[b.r][b.c];
+						minX = b.r;
+						minY = b.c;
+					}
+
 	
 				}
+				//System.out.println("??"+ minX + " "+minY);
 	
-				starts[time] = new Point(closestR, closestC);
-				map[closestR][closestC] = -1;
+				starts[time] = new Point(minX, minY);
+				map[minX][minY] = -1;
 				
 			}
 
 			//4. 시간 증가
 			time++;
+			
 					
 		}
 
@@ -155,33 +208,21 @@ public class Main {
 		public boolean inRange() {
 			return this.r >=1 && this.r <= n && this.c >= 1 && this.c <= n;
 		}
-	}
-	
-	static class Vertex implements Comparable<Vertex>{
-		int d;
-		int r;
-		int c;
 		
-		public Vertex(int d, int r, int c) {
-			this.d= d;
-			this.r=r;
-			this.c=c;
+		@Override
+		public boolean equals(Object o) {
+			if(o instanceof Point) {
+				Point p = (Point) o;
+				return this.r == p.r && this.c == p.c;
+			}
+			return false;
 		}
 		
 		@Override
-		public int compareTo(Vertex v) {
-			if(this.d != v.d) {
-				return Integer.compare(this.d, v.d);
-				
-			}else if(this.r != v.r) {
-				return Integer.compare(this.r , v.r);
-				
-			}else {
-				return Integer.compare(this.c, v.c);
-			}
+		public int hashCode() {
+			return Objects.hash(r,c);
 		}
-		
-		
 	}
+	
 	
 }
