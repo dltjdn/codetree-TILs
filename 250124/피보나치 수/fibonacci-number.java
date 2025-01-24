a2bb85654003ff474e69bf98c8bf16914804e7f1
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-01-24 09:08:09 +0900

    Title: 피보나치 수, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 250124/피보나치 수/fibonacci-number.java
+++ 250124/피보나치 수/fibonacci-number.java
@@ -2,14 +2,20 @@ import java.util.*;
 
 public class Main {
     public static int n;
-    public static int[] dp = new int[45];
+    public static int[] dp;
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        dp = new int[n+1];
+
+        if(n == 1){
+            System.out.println(1);
+            return;
+        }
 
         dp[1] = 1;
-        dp[2] = 1;
+        dp[2] = 1; // n=1 일때 에러
         for(int i=3; i<=n; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
