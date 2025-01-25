732b0aecee734a490bb3c71643762ae1a009d37d
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-01-25 10:09:51 +0900

    Title: 정수 사각형 최대 합, Time: 312ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 250125/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Main {
+    public static int n;
+    public static int[][] grid;
+    public static int[][] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        grid = new int[n][n];
+        dp = new int[n][n];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        init();
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = Math.max(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j]);
+            }
+        }
+
+        int answer = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){ 
+            answer = Math.max(answer, dp[n-1][i]);
+        }
+        System.out.println(answer);
+
+    }
+
+    public static void init(){
+        dp[0][0] = grid[0][0];
+
+        for(int i=1; i<n; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+
+        for(int j=1; j<n; j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+
+    }
+}
