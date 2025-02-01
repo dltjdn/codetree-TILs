9c8c4d9b72493749b193b65a2e03ed825ed0c836
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-02 03:37:15 +0900

    Title: 가장 짧은 부분합, Time: 415ms, Memory: 23MB, Status: Passed - CodeTree

--- 250202/가장 짧은 부분합/shortest-subtotal.java
+++ 250202/가장 짧은 부분합/shortest-subtotal.java
@@ -1,14 +1,13 @@
 import java.util.*;
 public class Main {
     //** 투포인터 : 2개의 포인터가 한 방향으로만 계속 전진
-    public static int n;
-    public static long s;
+    public static int n,s;
     public static int[] arr;
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        s = sc.nextLong();
+        s = sc.nextInt();
         arr = new int[n];
 
         for(int i=0; i<n; i++){
@@ -16,21 +15,25 @@ public class Main {
         }
 
         int j = 0;
-        long sum = 0;
+        int sum = 0;
         int ans = Integer.MAX_VALUE;
         for(int i=0; i<n; i++){
-            while(j < n && sum + arr[j] < s){
+            while(j < n && sum < s){
                 sum += arr[j];
                 j++;
             }
 
-            if(i == 0 && j == n && sum + arr[j] < s){
-                ans = -1;
+            if(sum < s){
+                break;
             }
 
-            ans = Math.min(j-i+1, ans);
+            ans = Math.min(j-i, ans);
 
             sum -= arr[i];
+        }
+
+        if(ans == Integer.MAX_VALUE){
+            ans = -1;
         }
 
         System.out.println(ans);
