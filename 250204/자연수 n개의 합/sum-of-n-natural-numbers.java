bda1d4eb2ac07b0e0f36e044f14106bb18afb511
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-04 13:54:13 +0900

    Title: 자연수 n개의 합, Time: 114ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250204/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static int s;
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        s = sc.nextInt();
+
+        long left = 1;
+        long right = (int)1e9;
+        long maxNum = 1;
+        while(left <= right){
+            long mid = (left + right) / 2;
+            
+            if(mid * (mid+1) / 2 <= s){
+                left = mid + 1;
+                maxNum = Math.max(maxNum, mid);
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(maxNum);
+    }
+}
