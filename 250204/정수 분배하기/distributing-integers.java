edd1a7f3d617746c076e4046eef4952dfb7fa92c
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-04 16:51:55 +0900

    Title: 정수 분배하기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250204/정수 분배하기/distributing-integers.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main {
+    public static int n,m;
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 1;
+        int right = 100000;
+        int ans = Integer.MIN_VALUE;
+        while(left <= right){
+            int mid = (left+right)/2;
+
+            if(isPossible(mid)){
+                left = mid + 1;
+                ans = Math.max(ans, mid);
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+
+    public static boolean isPossible(int maxNum){
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] < maxNum) return false;
+
+            cnt += arr[i]/maxNum;
+
+            if(cnt >= m){
+                return true;
+            }
+        }
+
+        return cnt >= m;
+    }
+}
