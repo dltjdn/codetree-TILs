68d3589bba6b19a3c8316e2a4066f8d341f7f490
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-06 01:50:05 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 220ms, Memory: 13MB, Status: Passed - CodeTree

--- 250206/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
+++ 250206/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -22,11 +22,16 @@ public class Main {
 
 
         for(int i=1; i<=n; i++){
-            dist[i] = (int)1e9;
+            dist[i] = (int)1e9;  // INT_MAX 그 자체로 설정하면
+        // 후에 덧셈 진행시 overflow가 발생할 수도 있으므로
+        // 적당히 큰 숫자로 적어줘야함에 유의
         }
 
         dist[1] = 0;
         for(int i=1; i<=n; i++){
+             // V개의 정점 중 
+            // 아직 방문하지 않은 정점 중
+            // dist값이 가장 작은 정점을 찾아줍니다.
             int minIdx = -1;
             for(int j=1; j<=n; j++){
                 if(visited[j]) continue;
