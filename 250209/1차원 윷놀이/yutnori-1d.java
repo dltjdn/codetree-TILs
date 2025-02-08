7cff9b1fc66efbd8780a0880f55a58d317da08c0
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-09 04:43:07 +0900

    Title: 1차원 윷놀이, Time: 151ms, Memory: 11MB, Status: Passed - CodeTree

--- 250209/1차원 윷놀이/yutnori-1d.java
+++ 250209/1차원 윷놀이/yutnori-1d.java
@@ -35,8 +35,6 @@ public class Main {
         }
 
         for(int i=0; i<k; i++){
-            if(step[i] > m-1) continue; // ** 조건추가
-
             step[i] += nums[idx];
                 
             backtrack(idx+1);
