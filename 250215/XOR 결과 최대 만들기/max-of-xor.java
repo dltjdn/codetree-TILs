0549e164e9d35b53df59636e4681f42dbdc3db3b
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-15 17:21:11 +0900

    Title: XOR 결과 최대 만들기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250215/XOR 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main {
+    public static int n,m;
+    public static int[] nums;
+    public static List<Integer> path = new ArrayList<>();
+    public static int answer = Integer.MIN_VALUE;
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        nums = new int[n];
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        backtrack(0);
+
+        System.out.println(answer);
+    }
+
+    public static void backtrack(int start){
+        if(path.size() == m){
+            int maxNum = 1;
+            for(int i=0; i<m; i++){
+                maxNum ^= path.get(i);
+            }
+            answer = Math.max(answer, maxNum);
+            return;
+        }
+
+
+        for(int i=start; i<n; i++){
+            path.add(nums[i]);
+            backtrack(i+1);
+            path.remove(path.size()-1);
+        }
+    }
+}
