bbd5ceb5e5819a23a334ee8c04b0dbb9a59e17f9
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-24 01:11:18 +0900

    Title: 바구니 안의 사탕, Time: 1593ms, Memory: 85MB, Status: Passed - CodeTree

--- 250224/바구니 안의 사탕/candy-in-the-basket.java
+++ 250224/바구니 안의 사탕/candy-in-the-basket.java
@@ -25,7 +25,7 @@ public class Main {
         long right = min;
         long cnt = 0;
         long ans = 0;
-        for(long left=min; left<=max-2*k; left++){
+        for(long left=min; left<=max; left++){
             while(right <= Math.min(left+2*k, max)){
                 if(map.containsKey(right)){
                     cnt += map.get(right);
