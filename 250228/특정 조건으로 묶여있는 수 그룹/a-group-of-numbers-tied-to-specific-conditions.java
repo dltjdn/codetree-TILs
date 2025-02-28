087c930174850d45ac9b6cfd4790c59a05407d09
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-28 16:20:08 +0900

    Title: 특정 조건으로 묶여있는 수 그룹, Time: 727ms, Memory: 34MB, Status: Passed - CodeTree

--- 250228/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.java
+++ 250228/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.java
@@ -1,31 +1,50 @@
 import java.util.*;
 
 public class Main {
+    public static int[] L,R;
     public static Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] arr = new int[n];
+        L = new int[n];
+        R = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
         
         Arrays.sort(arr);
 
         // 1 5 5 9 10 12 14
-
-        // 최대 2개 뽑기
-        int right = 0;
-        for(int left=0; left<n; left++){
-            while(right < n && arr[right]-arr[left] <= k){
-                right++;
+        // L[i] : i일때 최대 그룹길이
+        int maxNum = Integer.MIN_VALUE;
+        int i=0; 
+        for(int j=1; j<n; j++){
+            while(i+1 <= j && arr[j]-arr[i] > k){
+                i++;
             }
-            pq.add(right-left);
+            maxNum = Math.max(j-i+1, maxNum);
 
-            if(right == n) break;
+            L[j] = maxNum;
         }
 
-        if(n==1) System.out.println(1);
-        else System.out.println(pq.remove() + pq.remove());
+        // R[i] : i일때 최대 그룹 길이
+        maxNum = Integer.MIN_VALUE;
+        i = n-1;
+        for(int j=n-1; j>=0; j--){
+            while(j <= i-1 && arr[i]-arr[j] > k){
+                i--;
+            }
+            maxNum = Math.max(i-j+1, maxNum);
+
+            R[j] = maxNum;
+        }
+
+        int ans = -1;
+        for(int idx=0; idx<n-1; idx++){
+            ans = Math.max(ans, L[idx] + R[idx+1]);
+        }
+        System.out.println(ans);
+    
     }
 }
