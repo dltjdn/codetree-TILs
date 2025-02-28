8073a04fe82461714123307c383dfea40455c3b5
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-28 18:51:28 +0900

    Title: 막대기 나누기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250228/막대기 나누기/rod-cutting.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] profit = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            profit[i] = sc.nextInt();
+        }
+        dp = new int[n+1];
+        
+        Arrays.fill(dp, -(int)1e9);
+        dp[0] = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i >= j){
+                    dp[i] = Math.max(dp[i], dp[i-j]+profit[j]);
+                }       
+            }
+        }
+        System.out.println(dp[n]);
+
+    }
+}
