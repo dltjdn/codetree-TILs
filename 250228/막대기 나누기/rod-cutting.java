cb4cffeb51ae09a7fa97fab5e873013bc8070c26
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-28 18:55:39 +0900

    Title: 막대기 나누기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 250228/막대기 나누기/rod-cutting.java
+++ 250228/막대기 나누기/rod-cutting.java
@@ -13,10 +13,8 @@ public class Main {
         Arrays.fill(dp, -(int)1e9);
         dp[0] = 0;
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(i >= j){
-                    dp[i] = Math.max(dp[i], dp[i-j]+profit[j]);
-                }       
+            for(int j=1; j<=i; j++){
+                dp[i] = Math.max(dp[i], dp[i-j]+profit[j]);       
             }
         }
         System.out.println(dp[n]);
