48d6fb7145ae9d84ccf4d842a4bfeb3c2c3e369d
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-28 17:27:10 +0900

    Title: 최대 거리의 점, Time: 3103ms, Memory: 52MB, Status: Time Limit Exceed - CodeTree

---
+++ 250228/최대 거리의 점/maximum-distance-point.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Main {
+    public static int n,m;
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int left = 0;
+        int right = (int)1e9;
+        int ans = Integer.MIN_VALUE;
+        while(left <= right){
+            int mid = (left+right)/2;
+
+            if(isPossible(mid)){
+                left = mid + 1;
+                ans = Math.max(ans, mid);
+            }else{
+                right = mid -1;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static boolean isPossible(int mid){
+        int cnt = 1;
+        int cur = arr[0];
+        int i = 0;
+        while(i < arr.length){
+            if(arr[i] - cur < mid){
+                i++;
+                continue;
+            }
+            cnt++;
+            cur = arr[i];
+        }
+
+        return cnt >= m;
+    }
+}
