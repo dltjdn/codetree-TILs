adc9d6e95aeedd66fe9bc0016eacfbc53a76ceb9
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-02-28 15:42:43 +0900

    Title: 특정 조건으로 묶여있는 수 그룹, Time: 195ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 250228/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        Arrays.sort(arr);
+
+        // 1 5 5 9 10 12 14
+
+        // 최대 2개 뽑기
+        int right = 0;
+        for(int left=0; left<n; left++){
+            while(right < n && arr[right]-arr[left] <= k){
+                right++;
+            }
+            pq.add(right-left);
+        }
+
+        if(n==1) System.out.println(1);
+        else System.out.println(pq.remove() + pq.remove());
+    }
+}
