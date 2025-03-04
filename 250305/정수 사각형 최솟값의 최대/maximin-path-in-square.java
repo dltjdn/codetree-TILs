ad5e4e5d275dc8497b939c8c1e448f75a7089d0a
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-03-05 01:23:03 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 345ms, Memory: 23MB, Status: Passed - CodeTree

--- 250305/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 250305/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -18,7 +18,7 @@ public class Main {
 
         for(int i=1; i<n; i++){
             for(int j=1; j<n; j++){
-                dp[i][j] = Math.max(dp[i][j], Math.min(matrix[i][j], Math.max(dp[i-1][j], dp[i][j-1])));
+                dp[i][j] = Math.min(matrix[i][j], Math.max(dp[i-1][j], dp[i][j-1]));
             }
         }
 
@@ -31,11 +31,11 @@ public class Main {
         dp[0][0] = matrix[0][0];
         
         for(int i=1; i<n; i++){
-            dp[i][0] = Math.max(dp[i][0], Math.min(dp[i-1][0], matrix[i][0]));
+            dp[i][0] = Math.min(dp[i-1][0], matrix[i][0]);
         }
 
         for(int j=1; j<n; j++){
-            dp[0][j] = Math.max(dp[0][j], Math.min(dp[0][j-1], matrix[0][j]));
+            dp[0][j] = Math.min(dp[0][j-1], matrix[0][j]);
         }
     }
 }
