c154ad6d156c4fe144c46da9fac6a750d435dc95
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-03-13 18:10:37 +0900

    Title: 컨베이어 벨트, Time: 192ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250313/컨베이어 벨트/conveyor-belt.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int[] top = new int[n];
+        int[] bottom = new int[n];
+        for (int i = 0; i < n; i++) {
+            top[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            bottom[i] = sc.nextInt();
+        }
+
+        // 시간
+        while(t --> 0){
+            int temp = top[n-1];
+            for(int i=n-1; i>0; i--){
+                top[i] = top[i-1];
+            }
+            top[0] = bottom[n-1];
+
+            for(int i=n-1; i>0; i--){
+                bottom[i] = bottom[i-1];
+            }
+            bottom[0] = temp;
+        }
+      
+
+        for(int i=0; i<n; i++){
+            System.out.print(top[i] + " ");
+        }
+        System.out.println();
+        for(int i=0; i<n; i++){
+            System.out.print(bottom[i] + " ");
+        }
+        
+    }
+
+}
