b5fd84807f2066216b63c12b3357483e422689f1
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-03-15 00:13:06 +0900

    Title: 최소 통과 시간, Time: 242ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 250315/최소 통과 시간/minimum-transit-time.java
+++ 250315/최소 통과 시간/minimum-transit-time.java
@@ -10,11 +10,11 @@ public class Main {
         for (int i = 0; i < m; i++)
             arr[i] = sc.nextInt();
        
-        int left = 0;
-        int right = (int)1e10;
-        int ans = Integer.MAX_VALUE;
+        long left = 0;
+        long right = (int)1e14; // **long 고려하기
+        long ans = Integer.MAX_VALUE;
         while(left <= right){
-            int mid = (left+right)/2; // 최소 시간
+            long mid = (left+right)/2; // 최소 시간
 
             if(isPossible(mid)){
                 right = mid - 1;
@@ -27,8 +27,8 @@ public class Main {
         System.out.println(ans);
     }
 
-    public static boolean isPossible(int mid){
-        int sum = 0;
+    public static boolean isPossible(long mid){
+        long sum = 0;
         for(int i=0; i<m; i++){
             sum += (mid / arr[i]);
         }
