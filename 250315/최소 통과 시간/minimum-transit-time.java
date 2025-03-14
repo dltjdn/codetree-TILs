bf39d0d7f679d6d23b824b814e3c829030ffd8e7
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-03-15 00:08:17 +0900

    Title: 최소 통과 시간, Time: 3006ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 250315/최소 통과 시간/minimum-transit-time.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main {
+    public static int n,m;
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[m];
+        for (int i = 0; i < m; i++)
+            arr[i] = sc.nextInt();
+       
+        int left = 0;
+        int right = (int)1e14;
+        int ans = Integer.MAX_VALUE;
+        while(left <= right){
+            int mid = (left+right)/2; // 최소 시간
+
+            if(isPossible(mid)){
+                right = mid - 1;
+                ans = Math.min(ans, mid);
+            }else{
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static boolean isPossible(int mid){
+        int sum = 0;
+        for(int i=0; i<m; i++){
+            sum += (mid / arr[i]);
+        }
+        return sum >= n;
+    }
+
+}
