35872ebcd41ae951ab302d4eea58a1077808c967
dltjdn <78267146+dltjdn@users.noreply.github.com>
2025-03-14 23:37:02 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250314/양쪽 모두 존재하는 구간/both-exist-section.java
+++ 250314/양쪽 모두 존재하는 구간/both-exist-section.java
@@ -1,47 +1,41 @@
 import java.util.*;
 public class Main {
-    public static Map<Integer,Integer> map = new HashMap<>();
-    public static boolean[] visited;
+    public static Map<Integer,Integer> in = new HashMap<>();
+    public static Map<Integer,Integer> out = new HashMap<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[n];
-        
-        visited = new boolean[m+1];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
+            out.put(arr[i], out.getOrDefault(arr[i], 0)+1);
         }
        
         int ans = Integer.MAX_VALUE;
         int right = 0;
         for(int left=0; left<n; left++){
             // 1. 1~M 존재할때까지 구간 늘리기
-            while(right<n && map.keySet().size() < m){
-                map.put(arr[right], map.getOrDefault(arr[right], 0)+1);
+            while(right<n && in.keySet().size() < m){
+                in.put(arr[right], in.getOrDefault(arr[right], 0)+1);
+                
+                if(out.get(arr[right]) <= 1) out.remove(arr[right]);
+                else out.put(arr[right], out.get(arr[right])-1);
+                
                 right++;
             }
             //System.out.println(left+ " "+right);
             if(right == n) break;
           
             //2. 구간 밖에서 1~M 존재 확인 
-            Arrays.fill(visited, false);
-
-            for(int i=0; i<left; i++){
-                visited[arr[i]] = true;
+            if(in.keySet().size() == m && out.keySet().size() == m){
+                ans = Math.min(ans, right-left);
             }
-            for(int i=right; i<n; i++){
-                visited[arr[i]] = true;
-            }
-            boolean flag2 = true;
-            for(int i=1; i<=m; i++){
-                if(!visited[i]) flag2 = false;
-            }
-            if(flag2) ans = Math.min(ans, right-left);
             
             // 3. left 구간에 포함된 수 제거 
-            if(map.get(arr[left]) <= 1) map.remove(arr[left]);
-            else map.put(arr[left], map.get(arr[left])-1);
+            if(in.get(arr[left]) <= 1) in.remove(arr[left]);
+            else in.put(arr[left], in.get(arr[left])-1);
+            out.put(arr[left], out.getOrDefault(arr[left], 0)+1);
         }
 
         if(ans == Integer.MAX_VALUE) ans = -1;
